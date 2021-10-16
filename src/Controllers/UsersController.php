0e5ed6093aa137de2570b20ee99b32c4ea2298e1
ryinner <vinograd18062002@gmail.com>
2021-10-16 16:29:30 +0500

    Login и Logout в UsersController

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -9,17 +9,18 @@ namespace MasterOk\Controllers;
 class UsersController
 {
     /**
-     * Функция для регистрации, которая базируется на классе DataBaseController
-     *
-     * @return void
+     * Функция для регистрации, которая использует @class DataBaseController
+     * Проверяет уникальность логина
+     * 
+     * @return success
      */
     public function registration()
     {
-        $post= $_POST;
+        $post     = $_POST;
 
-        $name = $post['name'];
-        $login = $post['login'];
-        $email = $post['email'];
+        $name     = $post['name'];
+        $login    = $post['login'];
+        $email    = $post['email'];
         $password = $post['password'];
 
         $db = new DataBaseController();
@@ -44,13 +45,51 @@ class UsersController
         }
     }
 
+    /**
+     * Функция входа в аккаунт, через проверку пароля и логина
+     * Использует внутри @class DataBaseController
+     *
+     * @return success
+     */
     public function login()
     {
+        $post     = $_POST;
 
+        $login    = $post['login'];
+        $password = $post['password'];
+
+        $db = new DataBaseController;
+
+        $sql = "SELECT password FROM users WHERE login = :login";
+        $query = $db->pdo->prepare($sql);
+        $query->execute(['login' => $login]);
+        $query = $query->fetchAll();
+
+        if ($query !== array()) {
+
+            foreach ($query as $row) {
+                $hash = $row['password'];
+            }
+
+            if (password_verify($password,$hash)) {
+                $_SESSION['login'] = $login;
+                return json_encode(['success' => 'true']);
+            } else {
+                return json_encode(['success' => 'false']);
+            }
+
+        } else {
+            return json_encode(['success' => 'false']);
+        }
     }
 
+    /**
+     * Выход из аккаунта
+     *
+     * @return void
+     */
     public function logout()
     {
-        
+        session_destroy();
     }
 }
